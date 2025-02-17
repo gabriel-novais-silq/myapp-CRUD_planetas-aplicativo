8e0c13d9318803b9378e992aa4b856445cac9674
gabrielnovaissilqueira5 <gabrielnovaissilqueira5@gmail.com>
2025-02-17 16:14:29 +0000

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,130 @@
+//IMPORTAÇÕES
+import 'package:flutter/material.dart';
+
+import 'controles/controle_planeta.dart';
+import 'modelos/planetas.dart';
+import 'telas/telas_planetas.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Planetas',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 7, 25, 106)),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Aplicativo dos Planeta'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+//INICIO DO CRUD
+class _MyHomePageState extends State<MyHomePage> {
+  final ControlePlaneta _controlePlaneta = ControlePlaneta();
+  List<Planeta> _planetas = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _atualizarPlanetas();
+  }
+
+  Future<void> _atualizarPlanetas() async {
+    final resultado = await _controlePlaneta.lerPlanetas();
+    setState(() {
+      _planetas = resultado;
+    });
+  }
+
+  void _incluirPlaneta(BuildContext context) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder:
+            (context) => TelaPlaneta(
+              isIncluir: true,
+              planeta: Planeta.vazio(),
+              onFinalizado: () {
+                _atualizarPlanetas();
+              },
+            ),
+      ),
+    );
+  }
+
+  void _alterarPlaneta(BuildContext context, Planeta planeta) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder:
+            (context) => TelaPlaneta(
+              isIncluir: false,
+              planeta: planeta,
+              onFinalizado: () {
+                _atualizarPlanetas();
+              },
+            ),
+      ),
+    );
+  }
+
+  void _excluirPlaneta(int id) async {
+    await _controlePlaneta.excluirPlaneta(id);
+    _atualizarPlanetas();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: ListView.builder(
+        itemCount: _planetas.length,
+        itemBuilder: (context, index) {
+          final planeta = _planetas[index];
+          return ListTile(
+            title: Text(planeta.nome),
+            subtitle: Text(planeta.apelido!),
+            trailing: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                IconButton(
+                  icon: const Icon(Icons.edit),
+                  onPressed: () => _alterarPlaneta(context, planeta),
+                ),
+                IconButton(
+                  icon: const Icon(Icons.delete),
+                  onPressed: () => _excluirPlaneta(planeta.id!),
+                ),
+              ],
+            ),
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          _incluirPlaneta(context);
+        },
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
